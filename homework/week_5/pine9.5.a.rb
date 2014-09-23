29a8eb582fecc982e5fe3b3ff0babd1d97328148
katelynjms302 <Katelyn1851>
2014-09-22 20:04:19 -0400

    week 5 a

---
+++ homework/week_5/pine9.5.a.rb
@@ -0,0 +1,17 @@
+def do you like baseball?
+    while true
+        puts question
+        reply = gets.chomp
+        if reply == "yes"
+            return true
+        end
+        if reply == "no"
+            return false
+        end
+        # If we got this far, then we are going to loop
+        # and ask the question again
+        puts "Please answer "yes" or "no" ."
+    end
+end
+likes_it = ask "Do you like baseball?"
+puts likes_it
